4857cc49ef816d710d4d158f2b78aab3a7760cc0
Leonardorossato <leonardorossato21@gmail.com>
2023-08-30 16:15:27 -0300

    todos serviços pronto

--- src/fornecedor/fornecedor.service.ts
+++ src/fornecedor/fornecedor.service.ts
@@ -1,26 +1,110 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Fornecedor } from './entities/fornecedor.entity';
+import { Repository } from 'typeorm';
 import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
 
 @Injectable()
 export class FornecedorService {
-  create(createFornecedorDto: CreateFornecedorDto) {
-    return 'This action adds a new fornecedor';
+  constructor(
+    @InjectRepository(Fornecedor)
+    private readonly fornecedorRepository: Repository<Fornecedor>,
+  ) {}
+
+  async create(dto: CreateFornecedorDto) {
+    try {
+      const fornecedor = await this.fornecedorRepository.create(dto);
+      if (
+        (fornecedor.contatoNome && fornecedor.contatoEmail == null) ||
+        (fornecedor.contatoNome && fornecedor.contatoEmail == '')
+      ) {
+        throw new HttpException(
+          'Os campos de email e nome do fornecedor não podem esta vazios ou nullo',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      await this.fornecedorRepository.save(fornecedor);
+      return {
+        razaoSocial: dto.razao_Social,
+        endereco: dto.endereco,
+        contatos: [dto.contatoNome, dto.contatoEmail],
+      };
+    } catch (error) {
+      throw new HttpException(
+        'Erro em algum campo para criar este fornecedor',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all fornecedor`;
+  async findAll() {
+    try {
+      const fornecedores = await this.fornecedorRepository.find();
+      return fornecedores;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao encontrar todos os fornecedores',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fornecedor`;
+  async findOne(id: number) {
+    try {
+      const fornecedores = await this.fornecedorRepository.findOneBy({
+        id: id,
+      });
+      if (!fornecedores) {
+        throw new HttpException(
+          `Fornecedor com este id: ${fornecedores.id} não foi encontrado`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return fornecedores;
+    } catch (error) {
+      throw new HttpException(
+        'Fornecedor com este id não existe',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateFornecedorDto: UpdateFornecedorDto) {
-    return `This action updates a #${id} fornecedor`;
+  async update(id: number, dto: UpdateFornecedorDto) {
+    try {
+      const fornecedor = await this.fornecedorRepository.findOneBy({ id: id });
+      if (!fornecedor) {
+        throw new HttpException(
+          `Fornecedor com este id: ${id} não foi encontrado`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.fornecedorRepository.update(id, dto);
+      return { message: 'Dados do fornecedor atualizados com sucesso.' };
+    } catch (error) {
+      throw new HttpException(
+        'Não foi possivel atulizar este fornecedor',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fornecedor`;
+  async delete(id: number) {
+    try {
+      const fornecedor = await this.fornecedorRepository.findOneBy({ id: id });
+      if (!fornecedor) {
+        throw new HttpException(
+          `Fornecedor com este id: ${fornecedor.id} não foi encontrado`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.fornecedorRepository.delete(id);
+      return { message: 'Fornecedor removido do sistema com sucesso' };
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao remover este fornecedor',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
