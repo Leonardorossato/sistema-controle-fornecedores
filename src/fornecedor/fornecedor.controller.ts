b0fbb6af272316557edf7c73b7d5a68d04d6ae86
Leonardorossato <leonardorossato21@gmail.com>
2023-08-30 16:15:32 -0300

    rotas ok

--- src/fornecedor/fornecedor.controller.ts
+++ src/fornecedor/fornecedor.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { FornecedorService } from './fornecedor.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
 import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
+import { FornecedorService } from './fornecedor.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('fornecedor')
+@ApiTags('Fornecedor')
 export class FornecedorController {
   constructor(private readonly fornecedorService: FornecedorService) {}
 
-  @Post()
-  create(@Body() createFornecedorDto: CreateFornecedorDto) {
-    return this.fornecedorService.create(createFornecedorDto);
+  @Post('/create')
+  async create(@Body() createFornecedorDto: CreateFornecedorDto) {
+    return await this.fornecedorService.create(createFornecedorDto);
   }
 
-  @Get()
-  findAll() {
-    return this.fornecedorService.findAll();
+  @Get('/all')
+  async findAll() {
+    return await this.fornecedorService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fornecedorService.findOne(+id);
+  async findOne(@Param('id') id: number) {
+    return await this.fornecedorService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFornecedorDto: UpdateFornecedorDto) {
-    return this.fornecedorService.update(+id, updateFornecedorDto);
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateFornecedorDto: UpdateFornecedorDto,
+  ) {
+    return await this.fornecedorService.update(id, updateFornecedorDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fornecedorService.remove(+id);
+  async remove(@Param('id') id: number) {
+    return await this.fornecedorService.delete(id);
   }
 }
