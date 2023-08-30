dbdc18eb8140f861d0ad1f008e88ffa7b890e3e8
Leonardorossato <leonardorossato21@gmail.com>
2023-08-30 13:42:17 -0300

    swagger

--- src/fornecedor/dto/create-fornecedor.dto.ts
+++ src/fornecedor/dto/create-fornecedor.dto.ts
@@ -1,1 +1,15 @@
-export class CreateFornecedorDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateFornecedorDto {
+  @ApiProperty({ nullable: false, type: String })
+  razao_Social: string;
+
+  @ApiProperty({ nullable: false, type: String })
+  endereco: string;
+
+  @ApiProperty({ nullable: false, type: String })
+  contatoNome: string;
+
+  @ApiProperty({ nullable: false, type: String })
+  contatoEmail: string;
+}
