733b4a7f3279085086316f85d7f0b889a7f3ff59
Leonardorossato <leonardorossato21@gmail.com>
2023-08-30 13:42:11 -0300

    colunas adicionadas

--- src/fornecedor/entities/fornecedor.entity.ts
+++ src/fornecedor/entities/fornecedor.entity.ts
@@ -1,1 +1,19 @@
-export class Fornecedor {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Fornecedor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, type: 'varchar' })
+  razao_Social: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  endereco: string;
+
+  @Column({ nullable: false, type: 'varchar', unique: true })
+  contatoNome: string;
+
+  @Column({ nullable: false, type: 'varchar', unique: true })
+  contatoEmail: string;
+}
